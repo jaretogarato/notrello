29316ffe884ac52cfe01a46497caf1b23923426c
Jared Gallardo <jaredgg@gmail.com>
2017-09-21 09:32:56 -0600

    statuses now sorting into five columns

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -17,8 +17,8 @@ class Items extends Component {
     const { items } = this.props;
     const { status } = this.state;
     let visible = items;
-    if(status)
-      visible = items.filter( item => item.status === status)
+    // if(status)
+    //   visible = items.filter( item => item.status === status)
 
     let col1 = items.filter( item => item.status == this.stati[0] )
     let col2 = items.filter( item => item.status == this.stati[1] )
@@ -26,33 +26,35 @@ class Items extends Component {
     let col4 = items.filter( item => item.status == this.stati[3] )
     let col5 = items.filter( item => item.status == this.stati[4] )
     // debugger
-    // this.stati 
+    // this.stati
 
-    return visible.map( item => {
-      return(
-        <Grid.Column key={item.id} computer={4} mobile={16} tablet={16}>
-          <Card style={styles.itemCard}>
-            <Card.Content>
-              <Card.Header>{item.title}</Card.Header>
-              <Card.Meta>
-                <span>{item.details}</span>
-                <p>
-                  <span>Status: {item.status}</span>
-                  <span>Priority: {item.priority}</span>
-                  <span>Color: {item.color}</span>
-                </p>
-              </Card.Meta>
-              <Card.Description style={styles.itemDescription}>
-                {item.description}
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Link to={`/items/${item.id}`}>View Item</Link>
-            </Card.Content>
-          </Card>
+
+    return [0,1,2,3,4].map( col =>
+        <Grid.Column key={col} computer={3} mobile={16} tablet={16}>
+          { items.filter( i => i.status === this.stati[col]).map( item =>
+              <Card style={styles.itemCard}>
+                <Card.Content>
+                  <Card.Header>{item.title}</Card.Header>
+                  <Card.Meta>
+                    <span>{item.details}</span>
+                    <p>
+                      <span>Status: {item.status}</span>
+                      <span>Priority: {item.priority}</span>
+                      <span>Color: {item.color}</span>
+                    </p>
+                  </Card.Meta>
+                  <Card.Description style={styles.itemDescription}>
+                    {item.description}
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Link to={`/items/${item.id}`}>View Item</Link>
+                </Card.Content>
+              </Card>
+            )
+          }
         </Grid.Column>
-      )
-    })
+    )
   }
 
   clearFilter = () => {
